001a7460669f701afc2764ca739faed69ce99e3a
The Belgian Girl <myriamhmdaoui@gmail.com>
2025-01-24 15:24:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ async function sendMessage() {
 
   // Ask n8n for a reply (replace YOUR_N8N_URL below!)
   try {
-    const response = await fetch('YOUR_N8N_WEBHOOK_URL', {
+    const response = await fetch('https://n8n-e2tg.onrender.com/webhook-test/3124586d-9fcc-42dc-8281-0fdc70704fc7', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ message: userInput }),
