4c6913f9bbc5ee86ebee304cfe1e631a32ce210f
The Belgian Girl <myriamhmdaoui@gmail.com>
2025-01-25 13:28:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ async function sendMessage() {
 
   try {
     // 1. Send request
-    const response = await fetch('https://n8n-e2tg.onrender.com/webhook-test/3124586d-9fcc-42dc-8281-0fdc70704fc7', {
+    const response = await fetch('https://n8n-e2tg.onrender.com/webhook/3124586d-9fcc-42dc-8281-0fdc70704fc7', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ message: userInput, url: userInput }),
